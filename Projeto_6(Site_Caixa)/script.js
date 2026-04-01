3907a4110e8709a1ba6d454ea702e93b732f13ff
VictorHMSforne <vmendessforne@gmail.com>
2026-04-01 20:18:42 -0300

    Aprimorando o script do HTML

--- Projeto_6(Site_Caixa)/script.js
+++ Projeto_6(Site_Caixa)/script.js
@@ -8,6 +8,42 @@ let carrinho = [];
 
 const  audio = new Audio("bip.mp3");
 
-window.onload = () =>{
+window.onload = () =>{  // Uma função que ao carregar a página ela coloca o foco no input cod
     document.getElementById("cod").focus();
+}
+
+function addProduto(){
+    const codValue = document.getElementById("cod");
+    const qtdValue = document.getElementById("qtd");
+
+    const codigo = codValue.value;
+    const quantidade = qtdValue.value;
+
+    if (!produto[codigo]) {
+        AlertItem();
+        return;
+    } 
+
+    const produtoBase = produto[codigo];
+    const item = {
+        nome:   produtoBase.nome, 
+        preco:  produtoBase.preco,
+        quantidade: quantidade,
+        subtotal: produtoBase.preco * quantidade
+    };
+
+    carrinho.push(item);
+    audio.currentTimte = 0;
+    audio.play();
+
+    atualizarTela();
+}
+
+function atualizarTela(){
+    const lista = getElementById("lista");
+
+    lista.innerHTML = "";
+
+    let total = 0;
+    
 }
